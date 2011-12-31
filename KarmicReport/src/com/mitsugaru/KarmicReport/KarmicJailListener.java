0421a265a85d3cb9466f6ac244b7456434d9a362
Mitsugaru <mitsugaru@gmail.com>
2011-12-31 17:10:48 -0500

    Full support for KarmicJail events being properly added to a player's report.

--- KarmicReport/src/com/mitsugaru/KarmicReport/KarmicJailListener.java
+++ KarmicReport/src/com/mitsugaru/KarmicReport/KarmicJailListener.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.bukkit.event.CustomEventListener;
+import org.bukkit.event.Event;
 
 import com.mitsugaru.karmicjail.JailEvent;
 
@@ -13,30 +14,36 @@ public class KarmicJailListener extends CustomEventListener {
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss");
 
-	public KarmicJailListener(KarmicReport kr)
-	{
+	public KarmicJailListener(KarmicReport kr) {
 		plugin = kr;
+	}
+
+	public void onCustomEvent(Event event) {
+		if(event instanceof JailEvent)
+		{
+			this.onJailEvent((JailEvent) event);
+		}
 	}
 
 	public void onJailEvent(JailEvent event)
 	{
-			//Grab dates
-			final String date = dateFormat.format(new Date()).toString();
-			//Create query
-			StringBuilder sb = new StringBuilder();
-			sb.append("INSERT INTO 'kr_reports' (playername,author,date,comment) VALUES('");
-			sb.append(event.name+"','" + event.jailer +"','" +date + "'");
-			if(event.reason.equals(""))
-			{
-				//Default reason
-				sb.append(",'Jailed'");
-			}
-			else
-			{
-				//Add reasons
-				sb.append(",'" + event.reason + "');");
-			}
-			//Send query
-			plugin.getLiteDB().standardQuery(sb.toString());
+		// Grab dates
+		final String date = dateFormat.format(new Date()).toString();
+		// Create query
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO 'kr_reports' (playername,author,date,comment) VALUES('");
+		sb.append(event.name + "','" + event.jailer + "','" + date + "'");
+		if (event.reason.isEmpty())
+		{
+			// Default reason
+			sb.append(",'Jailed');");
 		}
+		else
+		{
+			// Add reasons
+			sb.append(",'" + event.reason + "');");
+		}
+		// Send query
+		plugin.getLiteDB().standardQuery(sb.toString());
+	}
 }
